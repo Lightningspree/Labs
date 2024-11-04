c2f91d3781ca7b53447fd3cb21792777b4d1ff26
Jarvis Murray <jarvis011877@gmail.com>
2024-11-04 18:42:42 -0500

    JS Refesher, and cooling/heating labs, start of longest name lab

---
+++ LongestJS/script.js
@@ -0,0 +1,4 @@
+let name1 = "Jarvis Murray";
+let name2 = "Grand Chirpus"
+let name3 = "Lorem Ipsum";
+
---
+++ RefresherJS/script.js
@@ -0,0 +1,21 @@
+const firstName = "Jarvis";
+const lastName = "Murray";
+const fullName = firstName + " " + lastName;
+
+let population = 633218;
+
+let javaScriptIsCool = true;
+
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+console.log(fullName);
+console.log(population);
+console.log(population * 3);
+
+for (let i = 1; i <= 5; i += 1) {
+	console.log(i)
+}
+
+for (let i = 0; i <= 4; i += 1) {
+	console.log(i)
+}
