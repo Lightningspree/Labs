73121a279bc3cef4828dcfab8055c580b4f6395f
Lightningspree <34173032+Lightningspree@users.noreply.github.com>
2024-11-07 19:08:34 -0500

    Update CountingJS/script.js
    Co-authored-by: AJ Tran <31839316+ajtran303@users.noreply.github.com>

--- CountingJS/script.js
+++ CountingJS/script.js
@@ -28,8 +28,6 @@ do{
 while (i >= 1) ;
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let text = "";
-for(let x of numbers){
-	text += x;
-	console.log(text);
+for (const number of numbers) {
+    console.log(number);
 }
